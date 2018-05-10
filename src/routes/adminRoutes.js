8d8d85deb51b21de7fae8891cc05e67e531f2775
Nuno Morgado <scolpit@gmail.com>
2018-05-10 09:28:07 +0100

    13

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bookRouter = express.Router();
+
+function router(nav) {
+
+}
+
+module.exports = router;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const bookRouter = express.Router();
 
 function router(nav) {
