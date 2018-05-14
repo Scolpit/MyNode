597bc6da16db38b95af49289f0cd567d31959450
Nuno Morgado <scolpit@gmail.com>
2018-05-14 20:20:52 +0100

    22

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,15 +5,48 @@ const passport = require('passport');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
   authRouter.route('/signup')
     .post((req, res) => {
-      //  Create user
-      req.login(req.body, () => {
-        res.redirect('/auth/profile');
-      });
-    });
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
 
+      (async function AddUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+
+          debug(results);
+
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (e) {
+          debug(e.stack);
+        } finally {
+          client.close();
+        }
+      }());
+    });
+  authRouter.route('/signin')
+    .get((req, res) => {
+      res.render('signin', {
+        nav,
+        title: 'Sign In'
+      });
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/'
+    }));
   authRouter.route('/profile')
     .get((req, res) => {
       res.json(req.user);
