8a9d8b73e0f9b454d67eac4e68d5ec69345dd4a3
Nuno Morgado <scolpit@gmail.com>
2018-05-15 13:12:42 +0100

    26

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,81 @@
-function bookController(){
+const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('app:bookController');
 
+function bookController(nav) {
+  function getIndex(req, res) {
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected correctly');
+
+        const db = client.db(dbName);
+
+        const col = await db.collection('books');
+        const books = await col.find().toArray();
+
+        res.render(
+          'bookListView',
+          {
+            title: 'Library',
+            nav,
+            books
+          }
+        );
+      } catch (e) {
+        debug(e.stack);
+      } finally {
+        client.close();
+      }
+    }());
+  }
+  function getById(req, res) {
+    const { id } = req.params;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected correctly');
+
+        const db = client.db(dbName);
+
+        const col = await db.collection('books');
+        const book = await col.findOne({ _id: new ObjectID(id) });
+        debug(book);
+        res.render(
+          'bookView',
+          {
+            title: 'Library',
+            nav,
+            book
+          }
+        );
+      } catch (e) {
+        debug(e.stack);
+      } finally {
+        client.close();
+      }
+    }());
+  }
+  function middleware(req, res, next) {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  }
+
+  return {
+    getIndex,
+    getById,
+    middleware
+  };
 }
 
 module.exports = bookController;
